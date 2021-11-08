b11b4ae1bf4f93824eb28d6c6911034b3cfcce52
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-11-08 20:33:01 +0200

    Add files via upload

--- LUFFY.lua
+++ LUFFY.lua
@@ -3984,7 +3984,7 @@ keyboard.inline_keyboard = {
 {{text = ' اضغط لاضافه البوت لمجموعتك ' ,url="t.me/"..dofile("./Info.lua").botUserName.."?startgroup=start"}}, 
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/xxxcccvvbbnn/927&caption='=' .. URL.escape(Text).."&photo="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/sasa_boody&caption=' .. URL.escape(Text).."&photo="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 
 if text == 'لوكاس' or text == 'مطور لوكاس' then
